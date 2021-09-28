d44402d702ebae1bf71366916f6c576426fbb239
Rupert <rc250@st-andrews.ac.uk>
2021-09-28 18:46:36 +0100

    Now retrives a list of all bibs on a volume given a (relative) path

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,17 +1,39 @@
 use neon::prelude::*;
+use glob::glob;
+use std::path::PathBuf;
+use std::env;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    let path: Handle<JsValue> = cx.argument(0)?;
+fn search_volume(mut cx: FunctionContext) -> JsResult<JsString> {
+    let path_handle = cx.argument::<JsString>(0)?;
+    let path_list: Vec<PathBuf> = get_all_bibtex_files(path_handle.value(&mut cx))?;
 
-    let path_string = path.downcast::<JsString, FunctionContext>(&mut cx).unwrap();
+    for path in path_list {
+        println!("{}", path.display())
+    }
 
-    println!("{}", path_string.value(&mut cx));
+    Ok(path_handle)
+}
 
-    Ok(path_string)
+fn get_all_bibtex_files(file_path: String) -> NeonResult<Vec<PathBuf>> {
+    
+    let bibtex_glob_string = "**/*.bib";
+    let glob_path: String = format!("{}/{}", file_path, bibtex_glob_string);
+
+    let mut relative_path_list: Vec<PathBuf> = Vec::new();
+
+    for entry in glob(&glob_path).expect("Bad Glob Pattern") {
+        match entry {
+            Ok(entry_value) => 
+                relative_path_list.push(entry_value),
+            Err(_e) => ()
+        };
+    }    
+
+    Ok(relative_path_list)
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("searchVolume", search_volume)?;
     Ok(())
 }
