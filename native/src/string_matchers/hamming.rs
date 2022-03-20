5ecd9399f81d1bd0a9ccde97d75c6e4764222ecf
rupert648 <rc250@st-andrews.ac.uk>
2022-03-20 22:59:34 +0000

    cleaned hamming to make use of char iterator

--- native/src/string_matchers/hamming.rs
+++ native/src/string_matchers/hamming.rs
@@ -13,17 +13,13 @@ pub fn compute(str1: &str, str2: &str) -> i32 {
     }
 
     let mut count = (longer.len()-shorter.len()) as i32;
-    let mut i = 0;
 
-    while i < shorter.len() {
-        let shorter_char = shorter.chars().nth(i).unwrap();
+    for (i, shorter_char) in shorter.chars().enumerate() {
         let longer_char = longer.chars().nth(i).unwrap();
 
         if shorter_char != longer_char {
             count+=1;
         }
-
-        i+=1;
     }
 
     count
